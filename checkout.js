09241df0835ff3e767d71ce1e501b2099c0ad336
Davit Meparishvili <dmeparishvili@intermedia.com>
2024-07-26 13:03:47 +0400

    Basic template

---
+++ checkout.js
@@ -0,0 +1,30 @@
+// This is your test secret API key.
+const stripe = Stripe(
+  "pk_test_51PgJWHK4fDeIIHWZmno0otYwwjWcWtxZO7yK2agdk0YQN2aDssiyrXEyIjsfYRXPd0sq9PpVM5hUWCTBH9mM9XGs00WDDK27VM"
+);
+
+initialize();
+
+// Create a Checkout Session
+async function initialize() {
+  const fetchClientSecret = async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/stripe/create-checkout-session-subscription",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: "mefarishvilidavit@gmail.com",
+        }),
+      }
+    );
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
+
+  const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret,
+  });
+
+  // Mount Checkout
+  checkout.mount("#checkout");
+}
